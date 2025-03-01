baec6174a33266cfa49c62f94d925721ee65b7aa
Hasan <alihasancse6@gmail.com>
2025-03-01 17:44:08 +0600

    style and comment activity log section

--- js/script.js
+++ js/script.js
@@ -30,4 +30,46 @@ for (let btn of quantityElement) {
     quantity.innerText = newQuantity;
   });
 }
+const showed = document.getElementsByClassName('fix-mobile');
+for (let showing of showed) {
+  showing.addEventListener('click', function () {
+    document.getElementById('show').classList.remove("hidden")
+  })
+}
+const payed = document.getElementsByClassName('add-pay');
+for (let showing of payed) {
+  showing.addEventListener('click', function () {
+    document.getElementById('Pay').classList.remove('hidden');
+  })
+}
+const reactions = document.getElementsByClassName('reaction');
+for (let showing of reactions) {
+  showing.addEventListener('click', function () {
+    document.getElementById('reaction').classList.remove('hidden');
+  });
+}
+const videos = document.getElementsByClassName('fix-video');
+for (let showing of videos) {
+  showing.addEventListener('click', function () {
+    document.getElementById('video').classList.remove('hidden');
+  });
+}
+const integrates = document.getElementsByClassName('integrate');
+for (let showing of integrates) {
+  showing.addEventListener('click', function () {
+    document.getElementById('integrate').classList.remove('hidden');
+  });
+}
+const reviews = document.getElementsByClassName('review');
+for (let showing of reviews) {
+  showing.addEventListener('click', function () {
+    document.getElementById('review').classList.remove('hidden');
+  });
+}
 
+const clearHistory = document.getElementById('clear');
+for (let clears of clearHistory) {
+  clears.addEventListener('click', function () {
+    document.getElementsByClassName('fix-mobile').classList.remove;
+  })
+}
