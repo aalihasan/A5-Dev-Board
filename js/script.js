48a6070738fb492cf1967846cf64689ba0971182
Hasan <alihasancse6@gmail.com>
2025-03-01 16:12:26 +0600

    added the complete button and sum & subtraction

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ function disable(x) {
     alert('not')
   }
 }
+
+const quantityElement = document.querySelectorAll('#complete');
+for (let btn of quantityElement) {
+  btn.addEventListener("click", function (event) {
+    const amout = event.target.innertext === "+" ? 1 : -1;
+    const quantity = document.getElementById('total-number');
+    const current = parseInt(quantity.innerText);
+    const newQuantity = Math.max(0, current - amout);
+    quantity.innerText = newQuantity;
+  })
+}
+
+const quantityElemen = document.querySelectorAll('#complete');
+for (let btn of quantityElement) {
+  btn.addEventListener('click', function (event) {
+    const amout = event.target.innertext === '+' ? 1 : -1;
+    const quantity = document.getElementById('Task-number');
+    const current = parseInt(quantity.innerText);
+    const newQuantity = Math.max(0, amout + current);
+    quantity.innerText = newQuantity;
+  });
+}
+
